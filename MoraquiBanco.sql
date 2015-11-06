51656ad89b6391a3c0835387a0e38c9eaa4d3462
minatel <felipeminatel@hotmail.com>
2015-11-06 16:23:10 -0200

    Update MoraquiBanco.sql

--- MoraquiBanco.sql
+++ MoraquiBanco.sql
@@ -1,14 +1,16 @@
-﻿DROP TABLE IF EXISTS contrato;
+DROP TABLE IF EXISTS contrato; 
 DROP TABLE IF EXISTS atributos;
-DROP TABLE IF EXISTS moradia;
+DROP TABLE IF EXISTS moradia; 
 DROP TABLE IF EXISTS visita;
 DROP TABLE IF EXISTS gerenciador;
 DROP TABLE IF EXISTS locatario;
-DROP TABLE IF EXISTS locator;
+DROP TABLE IF EXISTS locator; 
 DROP TABLE IF EXISTS itens;
-DROP TABLE IF EXISTS usuario;
+DROP TABLE IF EXISTS usuario; 
 DROP SEQUENCE IF EXISTS seq_CodUsuario;
-DROP SEQUENCE IF EXISTS seq_CodItens;
+DROP SEQUENCE IF EXISTS seq_Codgerenciador;
+DROP SEQUENCE IF EXISTS seq_Codlocatario;
+DROP SEQUENCE IF EXISTS seq_Codlocator;
 DROP SEQUENCE IF EXISTS seq_CodMoradia;
 DROP SEQUENCE IF EXISTS seq_CodContrato;
 
@@ -24,56 +26,59 @@ CREATE TABLE Usuario(
 	RG int,
 	CPF int,
 	DataNasce date,
+	Senha varchar(50),
 	Endereco varchar(100),
 	CEP int,
 	Sexo varchar(20),
 	Situacao boolean,
+	Tipo varchar(20),
 	CONSTRAINT UNRg UNIQUE (Rg),
 	CONSTRAINT PKUsuario PRIMARY KEY(CodUsuario)
 );
 
-CREATE TABLE Gerenciador(
-	CodGerenciador int,
-	CodUsuario int,
-	CONSTRAINT PKGerenciador PRIMARY KEY (CodGerenciador),
-	CONSTRAINT FKGerenciador FOREIGN KEY (CodUsuario) REFERENCES Usuario(CodUsuario) ON DELETE CASCADE ON UPDATE CASCADE
-);
-
-CREATE TABLE Locatario(
-	CodLocatario int,
-	CodUsuario int,
-	Fumante boolean,
-	Alergias varchar(100),
-	CONSTRAINT PKLocatario PRIMARY KEY (CodLocatario),
-	CONSTRAINT FKLocatario FOREIGN KEY(CodUsuario) REFERENCES Usuario(CodUsuario)
-);
-
-CREATE TABLE Locator(
-	CodLocator int,
-	CodUsuario int,
-	CONSTRAINT PKLocator PRIMARY KEY(CodLocator),
-	CONSTRAINT FKLocator FOREIGN KEY(CodUsuario) REFERENCES Usuario(CodUsuario)
-);
-
-CREATE TABLE Visita(
-	CodLocatario int,
-	CodLocator int,
-	DataHora date,
-	Descricao varchar(200),
-	Status varchar(50),
-	CONSTRAINT PKVisita PRIMARY KEY(CodLocatario, CodLocator),
-	CONSTRAINT FKVisLocatario FOREIGN KEY (CodLocatario) REFERENCES Locatario(CodLocatario),
-	CONSTRAINT FKVisLocator FOREIGN KEY (CodLocator) REFERENCES Locator(CodLocator)
-);
-
-CREATE SEQUENCE seq_CodItens
+CREATE SEQUENCE seq_CodGerenciador
     INCREMENT 1
     MINVALUE 1
     MAXVALUE 999999999
     START 1
     CACHE 1;
+CREATE TABLE Gerenciador(
+	CodGerenciador INT DEFAULT NEXTVAL('seq_CodGerenciador'),
+	CodUsuario int,
+	CONSTRAINT PKGerenciador PRIMARY KEY (CodGerenciador),
+	CONSTRAINT FKGerenciador FOREIGN KEY (CodUsuario) REFERENCES Usuario(CodUsuario) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE SEQUENCE seq_CodLocatario
+    INCREMENT 1
+    MINVALUE 1
+    MAXVALUE 999999999
+    START 1
+    CACHE 1;
+CREATE TABLE Locatario(
+	CodLocatario INT DEFAULT NEXTVAL('seq_CodLocatario'),
+	CodUsuario int,
+	Fumante boolean,
+	Alergias varchar(100),
+	CONSTRAINT PKLocatario PRIMARY KEY (CodLocatario),
+	CONSTRAINT FKLocatario FOREIGN KEY(CodUsuario) REFERENCES Usuario(CodUsuario) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE SEQUENCE seq_CodLocator
+    INCREMENT 1
+    MINVALUE 1
+    MAXVALUE 999999999
+    START 1
+    CACHE 1;
+CREATE TABLE Locator(
+	CodLocator INT DEFAULT NEXTVAL('seq_CodLocator'),
+	CodUsuario int,
+	CONSTRAINT PKLocator PRIMARY KEY(CodLocator),
+	CONSTRAINT FKLocator FOREIGN KEY(CodUsuario) REFERENCES Usuario(CodUsuario) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
 CREATE TABLE Itens(
-	CodItens int DEFAULT NEXTVAL('seq_CodItens'),
+	CodItens int,
 	Nome varchar(50),
 	Quant int,
 	Descricao varchar(200),
@@ -87,7 +92,7 @@ CREATE SEQUENCE seq_CodMoradia
     START 1
     CACHE 1;
 CREATE TABLE Moradia(
-	CodMoradia int DEFAULT NEXTVAL('seq_CodMoradia'),
+	CodMoradia INT DEFAULT NEXTVAL('seq_CodMoradia'),
 	CodLocator int,
 	Endereco varchar(100),
 	CEP int,
@@ -97,15 +102,27 @@ CREATE TABLE Moradia(
 	QuantMorador int,
 	ValorUnitario float,
 	CONSTRAINT PKMoradia PRIMARY KEY(CodMoradia),
-	CONSTRAINT FKMoradia FOREIGN KEY (CodLocator) REFERENCES Locator(CodLocator)
+	CONSTRAINT FKMoradia FOREIGN KEY (CodLocator) REFERENCES Locator(CodLocator) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE Visita(
+	CodLocatario int,
+	CodMoradia int,
+	Datavisita date,
+	HoraVisita time, 
+	Descricao varchar(200),
+	Status varchar(50),
+	CONSTRAINT PKVisita PRIMARY KEY(CodLocatario, CodMoradia),
+	CONSTRAINT FKVisLocatario FOREIGN KEY (CodLocatario) REFERENCES Locatario(CodLocatario),
+	CONSTRAINT FKVisMoradia FOREIGN KEY (CodMoradia) REFERENCES Moradia(CodMoradia) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE Atributos(
 	CodItens int,
 	CodMoradia int,
 	CONSTRAINT FKAtributos PRIMARY KEY (CodItens,CodMoradia),
-	CONSTRAINT PKAtribItens FOREIGN KEY (CodItens) REFERENCES Itens(CodItens),
-	CONSTRAINT PKAtribMorad FOREIGN KEY(CodMoradia) REFERENCES Moradia(CodMoradia)
+	CONSTRAINT PKAtribItens FOREIGN KEY (CodItens) REFERENCES Itens(CodItens) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT PKAtribMorad FOREIGN KEY(CodMoradia) REFERENCES Moradia(CodMoradia)ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE SEQUENCE seq_CodContrato
@@ -115,15 +132,18 @@ CREATE SEQUENCE seq_CodContrato
     START 1
     CACHE 1;
 CREATE TABLE Contrato(
-	CodContrato int DEFAULT NEXTVAL('seq_CodContrato'),
+	CodContrato INT DEFAULT NEXTVAL('seq_CodContrato'),
 	CodMoradia int,
 	CodLocatario int,
+	CodLocator int,
 	Valor float,
-	Data date,
+	DataInic date,
+	Datafim date,
 	Especificacoes varchar(200),
 	CONSTRAINT PKContrato PRIMARY KEY (CodContrato),
-	CONSTRAINT FKContMorad FOREIGN KEY (CodMoradia) REFERENCES Moradia(CodMoradia),
-	CONSTRAINT FKContLocat FOREIGN KEY (CodLocatario) REFERENCES Locatario(CodLocatario)
+	CONSTRAINT FKContMorad FOREIGN KEY (CodMoradia) REFERENCES Moradia(CodMoradia) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT FKContLocat FOREIGN KEY (CodLocatario) REFERENCES Locatario(CodLocatario) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT FKContLocad FOREIGN KEY (CodLocator) REFERENCES Locator(CodLocator) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 
